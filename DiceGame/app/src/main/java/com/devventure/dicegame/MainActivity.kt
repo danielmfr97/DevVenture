2dea9e4f78e874c6dfd046dfdcaf9e582b31e70a
Daniel Ramos <danielmfr6@gmail.com>
2021-06-08 22:21:05 -0300

    Adicionar DiceGame

---
+++ DiceGame/app/src/main/java/com/devventure/dicegame/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.devventure.dicegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dado01 = findViewById<TextView>(R.id.dado01)
+        val dado02 = findViewById<TextView>(R.id.dado02)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            dado01.text = gerarNumeroAleatorio().toString()
+            dado02.text = gerarNumeroAleatorio().toString()
+        }
+    }
+
+    private fun gerarNumeroAleatorio() = (1..6).random()
+}

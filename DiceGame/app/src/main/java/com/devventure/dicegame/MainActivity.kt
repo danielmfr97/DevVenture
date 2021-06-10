93a73544cb261181f94925dd20d38c9f83e97c25
Daniel Ramos <danielmfr6@gmail.com>
2021-06-09 23:15:51 -0300

    Adicionar logs ao life cycle

--- DiceGame/app/src/main/java/com/devventure/dicegame/MainActivity.kt
+++ DiceGame/app/src/main/java/com/devventure/dicegame/MainActivity.kt
@@ -2,14 +2,17 @@ package com.devventure.dicegame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 
+private const val TAG = "MeuCicloVida"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "OnCreate")
 
         val listDices = listOf(R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3,
             R.drawable.dice_4, R.drawable.dice_5, R.drawable.dice_6)
@@ -22,6 +25,31 @@ class MainActivity : AppCompatActivity() {
             diceOne.setImageResource(listDices.random())
             diceTwo.setImageResource(listDices.random())
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "OnDestroy")
     }
 
     private fun getRandomNumber() = (1..6).random()

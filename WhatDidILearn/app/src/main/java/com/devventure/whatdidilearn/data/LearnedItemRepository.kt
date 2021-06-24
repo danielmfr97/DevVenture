cf2ccd1ac0aa7e5f4e598be27e81b2849d40b703
Daniel Farias <danielmfr6@gmail.com>
2021-06-24 19:40:16 -0300

    Utilizar suspend functions para requisições

--- WhatDidILearn/app/src/main/java/com/devventure/whatdidilearn/data/LearnedItemRepository.kt
+++ WhatDidILearn/app/src/main/java/com/devventure/whatdidilearn/data/LearnedItemRepository.kt
@@ -3,9 +3,12 @@ package com.devventure.whatdidilearn.data
 import androidx.lifecycle.LiveData
 import com.devventure.whatdidilearn.data.database.LearnedItemDao
 import com.devventure.whatdidilearn.entities.LearnedItem
+import java.util.*
 
 class LearnedItemRepository(private val dao: LearnedItemDao) {
     val learnedItems: LiveData<List<LearnedItem>> = dao.getAll()
 
-    fun addNewItem(item: LearnedItem) = dao.insert(item)
+    suspend fun addNewItem(item: LearnedItem) {
+        dao.insert(item)
+    }
 }

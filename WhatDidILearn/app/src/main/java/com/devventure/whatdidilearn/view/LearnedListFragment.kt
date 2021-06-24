93d55743fc582e9aae33b474c8cb78598a9cc7f7
Daniel Farias <danielmfr6@gmail.com>
2021-06-24 19:38:22 -0300

    Alterar fragmento principal para LearnedListFragment

--- WhatDidILearn/app/src/main/java/com/devventure/whatdidilearn/view/LearnedListFragment.kt
+++ WhatDidILearn/app/src/main/java/com/devventure/whatdidilearn/view/LearnedListFragment.kt
@@ -2,7 +2,7 @@ package com.devventure.whatdidilearn.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,31 +11,41 @@ import androidx.navigation.fragment.findNavController
 import com.devventure.whatdidilearn.R
 import com.devventure.whatdidilearn.databinding.FragmentHomeBinding
 import com.devventure.whatdidilearn.view.adapters.LearnedItemAdapter
-import com.devventure.whatdidilearn.viewmodel.HomeViewModel
+import com.devventure.whatdidilearn.viewmodel.LearnedItemViewModel
 
-class HomeFragment : Fragment() {
+class LearnedListFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: HomeViewModel by viewModel()
+
+    private val viewModel: LearnedItemViewModel by sharedViewModel()
+    lateinit var mAdapter: LearnedItemAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
 
-        val recycler = binding.learnedItensRecyclerView
-        val adapter = LearnedItemAdapter()
-        val learnedItens = viewModel.learnedItems
-        learnedItens.observe(viewLifecycleOwner, Observer {
-            adapter.learnedItens = it
-        })
-
-        recycler.adapter = adapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.fabAddItem.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_newItemFragment)
         }
+        initRecyclerView()
+        subscriberObservers()
+    }
 
-        return binding.root
+    private fun initRecyclerView() {
+        mAdapter = LearnedItemAdapter()
+        binding.learnedItensRecyclerView.adapter = mAdapter
+    }
+
+    private fun subscriberObservers() {
+        val learnedItens = viewModel.learnedItems
+        learnedItens.observe(viewLifecycleOwner, Observer {
+            mAdapter.learnedItens = it
+        })
     }
 }
